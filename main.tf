a0f5f0f033ebd2ce59f240de0666a9d958554230
laurammberk <laurammberk3105@gmail.com>
2026-09-09 01:03:31 +0000

    changed ns name

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "kubernetes_resource_quota_v1" "this" {
 resource "kubernetes_limit_range" "this" {
   metadata {
     name      = "limit_range"
-    namespace = kubernetes_namespace_v1.example.metadata[0].name
+    namespace = kubernetes_namespace_v1.this.metadata[0].name
   }
   spec {
     limit {
